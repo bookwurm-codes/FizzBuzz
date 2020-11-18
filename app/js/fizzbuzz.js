f080fff2fc1e04dadb8d206b52a9b14bdf16b573
Kenan Blackerby <kblackerby2@gmail.com>
2020-11-17 22:23:50 -0600

    Tweak styling Format results in table format to easily read

--- app/js/fizzbuzz.js
+++ app/js/fizzbuzz.js
@@ -1,18 +1,34 @@
 function FizzBuzz(fizz, buzz) {
-    let returnValue = "";
+    let returnValue = [];
 
     for(let i = 1; i <=100; i++) {        
-        returnValue += (((i%fizz == 0)?"Fizz":"") + ((i%buzz == 0)?"Buzz":"") || i) + " ";
+        returnValue[i] = (((i%fizz == 0)?"Fizz":"") + ((i%buzz == 0)?"Buzz":"") || i.toString());
     }
 
     return returnValue;
 }
 
-
 function display() {
-    let output = "";
     let fizz = document.getElementById("fizz").value;
     let buzz = document.getElementById("buzz").value;
+    let output = [];
+    let formatted = "";
+
     output = FizzBuzz(fizz, buzz);
-    document.getElementById("results").innerHTML = output;
+    formatted = formatAsTable(output, 5);
+    document.getElementById("results").innerHTML = formatted;
+}
+
+function formatAsTable(arr, columns) {
+    let output = "<tr>";
+
+    for (let index = 1; index < arr.length; index++) {
+        const element = arr[index];
+        output += `<td>${element}</td>`;
+        if (index % columns === 0) {
+            output += "</tr><tr>";
+        }
+    }
+
+    return output += "</tr>";
 }
